eaf228aa9e03c3b5ae373388b4ea67d6a2ddc487
rahul008xy <158485654+rahul008xy@users.noreply.github.com>
2024-03-19 15:31:11 +0530

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -66,7 +66,7 @@ def hrt(seconds, precision = 0):
 
 timer = Timer()
 
-# Powered By AIR PHEONIX
+# Powered By TIGER
 async def progress_bar(current, total, reply, start):
     if timer.can_send():
         now = time.time()
