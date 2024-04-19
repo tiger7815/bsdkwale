af163e0119e7da888669ce2ad4be5896b68e9856
rahul008xy <158485654+rahul008xy@users.noreply.github.com>
2024-04-19 15:10:46 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -151,11 +151,10 @@ async def account_login(bot: Client, m: Message):
             
             elif '/master.mpd' in url:
                 id =  url.split("/")[-2]
-                url =  "https://d26g5bnklkwsh4.cloudfront.net/" + id + "/master.m3u8"
+            elif 'd26g5bnklkwsh4.cloudfront.net' in url or 'd1d34p8vz63oiq.cloudfront.net' in url:
+                id =  url.split("/")[-2]
+                url =  "https://psitoffers.store/testkey.php?vid=" + id + "&quality=" + raw_text2
 
-            elif '/master.mpd' in url:                
-               url = f'https://master-extract-api-8180936ccf13.herokuapp.com/?url={url}'
-               
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
             name = f'{str(count).zfill(3)}) {name1[:60]}'
 
