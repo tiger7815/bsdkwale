5da134b2af8cc62dc54493b19a5497f76e83ba7b
TIGER <141488144+tiger7815@users.noreply.github.com>
2024-05-31 03:55:26 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,7 +2,7 @@ import os
 
 api_id = int(os.environ.get("API_ID", '27862677'))
 api_hash = os.environ.get("API_HASH", 'e343ce2c81b2b6c2c0d6bee58284e3bd')
-bot_token = os.environ.get("BOT_TOKEN", '6871505238:AAF0DiNwF2n5VmmLE77xwdb2zrZrgECWe24')
+bot_token = os.environ.get("BOT_TOKEN", '6891484332:AAHAiVZDQZc7CHW8SRYg_iVe-rC3e20_E2w')
 
 
 
