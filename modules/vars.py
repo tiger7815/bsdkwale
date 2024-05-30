5d53e4ef8901ed128713438dcc343bec1b669643
TIGER <141488144+tiger7815@users.noreply.github.com>
2024-05-31 02:26:06 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,13 +1,13 @@
 import os
 
-api_id = int(os.environ.get("API_ID", ''))
-api_hash = os.environ.get("API_HASH", '')
-bot_token = os.environ.get("BOT_TOKEN", '')
+api_id = int(os.environ.get("API_ID", '27862677'))
+api_hash = os.environ.get("API_HASH", 'e343ce2c81b2b6c2c0d6bee58284e3bd')
+bot_token = os.environ.get("BOT_TOKEN", '6871505238:AAF0DiNwF2n5VmmLE77xwdb2zrZrgECWe24')
 
 
 
 
-OWNER = int(os.environ.get("OWNER", ''))
+OWNER = int(os.environ.get("OWNER", '5881684718'))
 
 try:
     ADMINS=[]
