8b37a4408e80b7fe957865e6b654d99b3ab9cb49
rahul008xy <158485654+rahul008xy@users.noreply.github.com>
2024-06-01 02:26:06 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -102,8 +102,8 @@ async def account_login(bot: Client, m: Message):
 
     await editable.edit("Enter A Captio To Add Otherwise Send   **no**")
     input3: Message = await bot.listen(editable.chat.id)
-    raw_text3 = input3.text
-    await input3.delete(True)
+    raw_text3 = input.text
+    await input.delete(True)
     highlighter  = f"️ ⁪⁬⁮⁮⁮"
     if raw_text3 == 'no':
         MR = highlighter 
